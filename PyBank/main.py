1330c7f7bfac592c14bdaa6a8da849ba852b15af
Kunal Khirwar <kunalkhirwar@gmail.com>
2023-11-11 21:58:32 -0500

    done PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,8 +2,17 @@ import os
 import csv
 import math
 
+total_month = 0
 total_amount = 0
 change = 0
+next_value = 0
+value_list = []
+counter = 0
+sum = 0
+average_value = 0
+greatest_decrease = 0
+greatest_increase = 0
+counter1 = 0
 
 print("Financial Analysis")
 print("------------------------------")
@@ -14,14 +23,46 @@ with open(data_path , 'r') as datafile:
 
     csv_reader = csv.reader(datafile, delimiter=',')
     next(csv_reader)
-    
-    total_months = list(csv_reader)
-    print(f"Total Months: {len(total_months)}")
-    
-    for column in csv_reader:
-        total_amount += int(column[1])
+     
+    #  
+    for row in csv_reader:
+        total_amount += int(row[1])
+        total_month += 1
+    print(f"Total Months: {total_month}")
     print(f"Total: ${total_amount}")
-
+            
+    for row in csv_reader:
+        counter += 1
+        current_value = row[1]
+            
+        if counter == 1:
+            previous_value = current_value
+                                                    
+        else:
+            subtraction = int(current_value) - int(previous_value)
+            previous_value = current_value
+            value_list.append(subtraction)
+                
+    for values in value_list:
+        counter1 += 1
+        sum += values
+        average_value = round(int(sum)/len(value_list),2)
+        greatest_increase = max(value_list)
+        greatest_decrease = min(value_list)
+    print(f"Average Change: ${average_value}") 
+    print(f"Greatest Increase in Profits: (${greatest_increase})") 
+    print(f"Greatest Decrease in Profits: (${greatest_decrease})")      
     
+with open("Result.txt", "w") as result_file:
+    result_file.write("Financial Analysis\n")
+    result_file.write("----------------------------\n")
+    result_file.write(f"Total Months: {total_month}\n")
+    result_file.write(f"Total: ${total_amount}\n")
+    result_file.write(f"Average Change: ${average_value}\n")
+    result_file.write(f"Greatest Increase in Profits: (${greatest_increase})\n")
+    result_file.write(f"Greatest Decrease in Profits: (${greatest_decrease})\n")
     
 
+
+
+        
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,15 +3,56 @@ import csv
 
 csv_file = r"Resources\election_data.csv"
 
-print("Election Results")
-print("------------------------")
+candidate_name = {}
+candidate_list = []
+lines = []
+percent_vote = 0
+winner = 0
 
+# Open csv file and read it
 with open(csv_file, 'r') as datasheet:
     csvreader = csv.reader(datasheet, delimiter=",")
     next(csvreader)
 
-    total_votes = list(csvreader)
-    print(f"Total Votes: {len(total_votes)}")
-    print("------------------------")
+    #total number of votes
+    for row in csvreader:
+        if not row[2] in candidate_name.keys():         #this will check if the candidate already exists in the candidate_name dictionary 
+            candidate_name[row[2]] = 1                  #if candidate does not exist in candidate_name dictionary, candidate key will be created with '1' value
 
+        else:
+            candidate_name[row[2]] += 1                 #if candidate already exists in candidate_name dictionary, 1 will be added to it's existing value
     
+    print("Election Results")
+    print("------------------------")
+    print(f"Total Votes: {sum(candidate_name.values())}")
+    print("------------------------")
+    
+    #list of candidates who received votes, percent of votes each candidate won and total number of votes each candidate won
+    for candidate in candidate_name:
+        percent_vote = round(candidate_name[candidate]/sum(candidate_name.values())*100,3)
+        print(f"{candidate} : {percent_vote}%  ({candidate_name[candidate]})")
+
+        #used lines list to populate different rows for each candidate in the txt file
+        lines.append(candidate + " : " + str(percent_vote) + "%" + " (" + str(candidate_name[candidate]) + ")") 
+
+        candidate_list.append(candidate_name[candidate])
+
+    #winner of the election based on maximum votes received
+    #the following code separates the dictionary's values in a list, finds the position of the maximum value I have, and gets the key at that position. 
+    winner = (list(candidate_name.keys())[list(candidate_name.values()).index(max(candidate_list))])
+    
+    print("---------------------------")
+    print(f"Winner : {winner}")
+    print("---------------------------")
+
+    #writing to the result file
+    with open("Result.txt", "w") as final_file:
+        final_file.write("Election Results\n")
+        final_file.write("-------------------------\n")
+        final_file.write(f"Total Votes: {sum(candidate_name.values())}\n")
+        final_file.write("-------------------------\n")
+        for line in lines:
+            final_file.write(f"{line}\n")
+        final_file.write("-------------------------\n")
+        final_file.write(f"Winner: {winner}\n")
+        final_file.write("-------------------------\n")
