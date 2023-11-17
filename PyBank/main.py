ff477caea78a2a7cd785b228e1f85e56f7e9dd3f
Kunal Khirwar <kunalkhirwar@gmail.com>
2023-11-16 21:45:42 -0500

    done

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,8 +17,10 @@ my_list = []
 print("Financial Analysis")
 print("------------------------------")
 
+# Path to collect data from the Resources folder
 data_path = r"PyBank\Resources\budget_data.csv"
 
+# Read in the CSV file
 with open(data_path , 'r') as datafile:
 
     csv_reader = csv.reader(datafile, delimiter=',')
@@ -33,32 +35,36 @@ with open(data_path , 'r') as datafile:
             
         if counter == 1:                      # used 'if' statment to skip the first value as no value exists before to subtract
             previous_value = current_value
-            my_list.append(row[0])
+            my_list.append(row[0])              #used 'my_list' to capture the months in a list
                                                    
         else:
             # used'subtract' variable to store the difference between current value and previus value which changes with 'counter' every time 'for' loop iterates
             subtraction = int(current_value) - int(previous_value)
 
-            # 'current value' of present iteration becomes previus value for next iteration
+            # 'current value' of present iteration becomes previous value for next iteration
             previous_value = current_value 
                 
-            value_list.append(subtraction)
-            my_list.append(row[0])
-    print(my_list)       
+            value_list.append(subtraction)          #values from 'subtraction' variable are appened into 'values_list' list
+            my_list.append(row[0])                  #months from 'row' variable are appened into 'my_list' list
+       
     print(f"Total Months: {total_month}")
     print(f"Total: ${total_amount}")           
     
     for values in value_list:
         sum += values
         average_value = round((sum)/len(value_list),2)
+
     greatest_increase = max(value_list)
     greatest_decrease = min(value_list)
-    month_greatest_increase = my_list[value_list.index(max(value_list))+1]
-    month_greatest_decrease = my_list[value_list.index(min(value_list))+1]
+    
+    month_greatest_increase = my_list[value_list.index(max(value_list))+1]          # max returns the index of maximium value in 'values_list'
+    month_greatest_decrease = my_list[value_list.index(min(value_list))+1]          #t o get the index value in 'my_list', +1 added to the index of 'value_list'
+    
     print(f"Average Change: ${average_value}") 
     print(f"Greatest Increase in Profits: {month_greatest_increase} (${greatest_increase})") 
     print(f"Greatest Decrease in Profits: {month_greatest_decrease} (${greatest_decrease})")      
-    
+
+# Write in the txt file    
 with open("PyBank\Analysis\Result.txt", "w") as result_file:
     result_file.write("Financial Analysis\n")
     result_file.write("----------------------------\n")
