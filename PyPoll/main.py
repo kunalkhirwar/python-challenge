502e5bb157c3f44cddb469db50f4f0cbb9a71f33
Kunal Khirwar <kunalkhirwar@gmail.com>
2024-02-21 14:23:59 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -36,23 +36,25 @@ with open(csv_file, 'r') as datasheet:
         lines.append(candidate + " : " + str(percent_vote) + "%" + " (" + str(candidate_name[candidate]) + ")") 
 
         candidate_list.append(candidate_name[candidate])
-
+    
     #winner of the election based on maximum votes received
     #the following code separates the dictionary's values in a list, finds the position of the maximum value I have, and gets the key at that position. 
     winner = (list(candidate_name.keys())[list(candidate_name.values()).index(max(candidate_list))])
-    
-    print("---------------------------")
-    print(f"Winner : {winner}")
-    print("---------------------------")
+    print(list(candidate_name.keys())[1])
+    print([list(candidate_name.values()).index(max(candidate_list))])
 
-    #writing to the result file
-    with open("PyPoll\Analysis\Result.txt", "w") as final_file:
-        final_file.write("Election Results\n")
-        final_file.write("-------------------------\n")
-        final_file.write(f"Total Votes: {sum(candidate_name.values())}\n")
-        final_file.write("-------------------------\n")
-        for line in lines:
-            final_file.write(f"{line}\n")
-        final_file.write("-------------------------\n")
-        final_file.write(f"Winner: {winner}\n")
-        final_file.write("-------------------------\n")
+    # print("---------------------------")
+    # print(f"Winner : {winner}")
+    # print("---------------------------")
+
+    # #writing to the result file
+    # with open("PyPoll\Analysis\Result.txt", "w") as final_file:
+    #     final_file.write("Election Results\n")
+    #     final_file.write("-------------------------\n")
+    #     final_file.write(f"Total Votes: {sum(candidate_name.values())}\n")
+    #     final_file.write("-------------------------\n")
+    #     for line in lines:
+    #         final_file.write(f"{line}\n")
+    #     final_file.write("-------------------------\n")
+    #     final_file.write(f"Winner: {winner}\n")
+    #     final_file.write("-------------------------\n")
